e0bc16d109f33f7390356b1fa8333272b28104bf
schneiderdies <90262775+schneiderdies@users.noreply.github.com>
2022-06-18 16:44:11 +0300

    Update 42.py

--- 42.py
+++ 42.py
@@ -1,8 +1,11 @@
-a = input().split()
-print(a[0], end = '')
-for i in range(1, len(a)):
-    print(" ", end = '')
-    if ord(a[i][0]) >= ord('А') and ord(a[i][0]) <= ord('Я'):
-        print("NAME", end = '')
-    else:
-        print(a[i], end = '')
+x = int(input('Введите положительное число: \n'))
+c = 0
+d = []
+for i in range(2, x):
+    if a % i == 0:
+        d.append(i)
+if len(d) == 0:
+    print('-1')
+else:
+    for i in range(len(d)):
+        print(d[i])
